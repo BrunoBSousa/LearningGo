694c4dca037e61f94c7db68f8e0ee569812be200
Bruno Sousa <107301313+BrunoBSousa@users.noreply.github.com>
2023-08-09 16:07:37 +0100

    Rock paper Scissors game
    Added the basic logic of the game.

--- RPS.go
+++ RPS.go
@@ -9,19 +9,30 @@ func main(){
   c:= [3]string {"Rock","Paper","Scissors"}
 var name string
 var choise string
+
+
+//Asking for user's name and pick for the game
 fmt.Println("Hi Stanger, what is you name ?")
 fmt.Scan(&name)
 fmt.Printf("Hi,%v this is a game of Rock, Paper Scissors, please choose one of the following to begin the game\n",name)
 fmt.Scan(&choise)
-fmt.Println(choise)
-//fmt.Print(rand.Intn(3))
+//Generationg a number from 0 to 3 to use as index of the array
 r := c[rand.Intn(3)]
-//fmt.Println(r)
-//fmt.Println(c[rand.Intn(3)])
+//Doing the logic of the game to see if User won or computer won the game
 if choise == r {
-fmt.Println("empate")
-} else if choise == "Rock" && r == "Paper"{
-fmt.Println("perdeste")
+fmt.Println("It was a Draw, the computer also choose",r)
+} else if choise == "Rock" && r== "Paper"{
+fmt.Printf("Sorry you Lost, the computer picked %v \n",r)
+}else if choise == "Rock" && r == "Scissors"{
+fmt.Printf("Congratulations you Won, the computer picked %v \n",r)
+}else if choise == "Paper" && r == "Scissors"{
+fmt.Printf("Sorry you Lost, the computer picked %v \n",r)
+}else if choise =="Paper" && r == "Rock"{
+fmt.Printf("Congratulations you Won, the computer picked %v \n",r)
+}else if choise =="Scissors" && r == "Rock"{
+fmt.Printf("Sorry you Lost, the computer picked %v \n",r)
+}else{
+  fmt.Printf("Congratulations you Won, the computer picked %v \n",r)
 }
 }
 
